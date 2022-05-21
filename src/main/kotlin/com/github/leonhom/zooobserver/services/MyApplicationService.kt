7dc21539dd9d82a4ae7cae2a7708561ea2881827
GitHub Action <action@github.com>
2022-05-21 14:12:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/leonhom/zooobserver/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leonhom/zooobserver/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leonhom.zooobserver.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leonhom.zooobserver.MyBundle
 
 class MyApplicationService {
 
